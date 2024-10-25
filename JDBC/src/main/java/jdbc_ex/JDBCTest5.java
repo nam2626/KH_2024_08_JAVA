79844ae31f0ab35ff8452a5e28c3d7cbff328c37
nam2626 <nam2626@naver.com>
2024-10-25 15:46:49 +0900

    JDBC Test

---
+++ JDBC/src/main/java/jdbc_ex/JDBCTest5.java
@@ -0,0 +1,30 @@
+package jdbc_ex;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+import java.sql.*;
+
+public class JDBCTest5 {
+    public static void main(String[] args) {
+
+        try {
+            OracleDataSource ods = new OracleDataSource();
+            ods.setURL("jdbc:oracle:thin:@localhost:1521:xe");
+            ods.setUser("C##SCOTT");
+            ods.setPassword("TIGER");
+
+            try(Connection conn = ods.getConnection()) {
+                String sql = "SELECT * FROM PERSON";
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql);
+                while (rs.next()) {
+                    System.out.println(
+                            rs.getString("PID") + "/"
+                                    + rs.getString(2));
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
